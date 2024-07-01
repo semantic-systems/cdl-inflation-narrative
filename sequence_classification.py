b45344ce001b8cd02524047b78b65815acaaa70a
Junbo Huang <junbo.huang@uni-hamburg.de>
2024-07-01 16:41:32 +0200

    :sparkles: added dag prompt

--- sequence_classification.py
+++ sequence_classification.py
@@ -53,6 +53,7 @@ def run_llama3_vllm_inflation_1_1():
         prompts = [f"{prompt}\n{snippet}\nanswer:\n" for snippet in df["body"].values]
         outputs = llm.generate(prompts, sampling_params)
         generated_texts = [output.outputs[0].text for output in outputs]
+        df["prompts"] = prompts
         df[prediction_col] = generated_texts
         df.to_csv(Path(f"./outputs/llama3/inflation/1.1.inflation_has_cause_{year}.csv"), index=False)
 
@@ -71,6 +72,7 @@ def run_llama3_vllm_deflation_1_2():
         prompts = [f"{prompt}\n{snippet}\nanswer:\n" for snippet in df["body"].values]
         outputs = llm.generate(prompts, sampling_params)
         generated_texts = [output.outputs[0].text for output in outputs]
+        df["prompts"] = prompts
         df[prediction_col] = generated_texts
         df.to_csv(Path(f"./outputs/llama3/deflation/1.2.deflation_has_cause_{year}.csv"), index=False)
 
@@ -89,7 +91,8 @@ def run_llama3_vllm_change_in_prices_2_1():
         prompts = [f"{prompt}\n{snippet}\nanswer:\n" for snippet in df["body"].values]
         outputs = llm.generate(prompts, sampling_params)
         generated_texts = [output.outputs[0].text for output in outputs]
-        df[prediction_col] = generated_texts
+        df["prompts"] = prompts
+        df["answer_change_in_prices"] = generated_texts
         df.to_csv(Path(f"./outputs/llama3/change_in_prices/2.1.change_in_prices_{year}.csv"), index=False)
 
 def run_llama3_vllm_change_in_prices_2_2():
@@ -98,18 +101,41 @@ def run_llama3_vllm_change_in_prices_2_2():
     file = open(f"prompts/2.2.change_direction.txt", "r")
     prompt = file.read()
     for year in [2023, 2018, 2019, 2020, 2021, 2022]:
-        df_path = Path(f"./data/DJN/inflation_deflation_prices_{year}.csv")
+        df_path = Path(f"./outputs/llama3/change_in_prices/2.1.change_in_prices_{year}.csv")
         df = pd.read_csv(df_path)
         print(f"Year: {year}")
         print(f"Number of articles: {len(df)}")
-        df = df.drop_duplicates(subset=['body'])
-        print(f"Number of articles (deduplicated): {len(df)}")
-        prompts = [f"{prompt}\n{snippet}\nanswer:\n" for snippet in df["body"].values]
+        df = df.loc[df[prediction_col].str.startswith("Yes")]
+        df["prompts"] = 'User: ' + df['prompts'] + '\nSystem: ' + df['answer_change_in_prices']
+        prompts = [f"{prompt_history}\nUser: {prompt}\n\nSystem: " for prompt_history in df["prompts"].values]
         outputs = llm.generate(prompts, sampling_params)
         generated_texts = [output.outputs[0].text for output in outputs]
-        df[prediction_col] = generated_texts
+        df["prompts"] = prompts
+        df["answer_change_direction"] = generated_texts
         df.to_csv(Path(f"./outputs/llama3/change_in_prices/2.2.change_direction_{year}.csv"), index=False)
 
+def run_llama3_vllm_one_hop_dag_3_1():
+    llm = LLM(model="meta-llama/Meta-Llama-3-8B-Instruct", tensor_parallel_size=2)
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=500)
+    file = open(f"prompts/3.1.one_hop_dag.txt", "r")
+    prompt = file.read()
+    prev_file = open(f"prompts/1.1.inflation_has_cause.txt", "r")
+    prev_prompt = prev_file.read()
+    for year in [2023, 2018, 2019, 2020, 2021, 2022]:
+        df_path = Path(f"./outputs/llama3/inflation/1.1.inflation_has_cause_{year}.csv")
+        df = pd.read_csv(df_path)
+        prev_prompts = [f"{prev_prompt}\n{snippet}\nanswer:\n" for snippet in df["body"].values]
+        df["prompts"] = prev_prompts
+        print(f"Year: {year}")
+        print(f"Number of articles: {len(df)}")
+        df = df.loc[df[prediction_col].str.startswith("Yes")]
+        df["prompts"] = 'User: ' + df['prompts'] + '\nSystem: ' + df['response']
+        prompts = [f"{prompt_history}\nUser: {prompt}\n\nSystem: " for prompt_history in df["prompts"].values]
+        outputs = llm.generate(prompts, sampling_params)
+        generated_texts = [output.outputs[0].text for output in outputs]
+        df["prompts"] = prompts
+        df["answer_one_hop_dag"] = generated_texts
+        df.to_csv(Path(f"./outputs/llama3/one_hop_dag/3.1.one_hop_dag_{year}.csv"), index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -123,3 +149,5 @@ if __name__ == "__main__":
         run_llama3_vllm_change_in_prices_2_1()
     if args.exp == "direction_of_change":
         run_llama3_vllm_change_in_prices_2_2()
+    if args.exp == "one_hop_dag":
+        run_llama3_vllm_one_hop_dag_3_1()
