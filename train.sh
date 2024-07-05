2aeab082de092ee2501627e92ba91c87667f3fa4
Junbo Huang <junbo.huang@uni-hamburg.de>
2024-07-05 09:25:44 +0200

    :bug: encoding method

--- train.sh
+++ train.sh
@@ -2,4 +2,6 @@
 #SBATCH --gpus-per-node=2
 
 huggingface-cli login --token $HFTOKENS
-python sequence_classification.py deflation
+python sequence_classification.py one_hop_dag
+python sequence_classification.py change_of_prices
+python sequence_classification.py direction_of_change
