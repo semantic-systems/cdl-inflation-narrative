5d8dc321103b3c627c01a8f56f5c08a98e27b92a
Junbo Huang <junbo.huang@uni-hamburg.de>
2024-07-15 13:23:30 +0200

    :sparkles: analysis inflation

--- get_statistics.py
+++ get_statistics.py
@@ -1,8 +1,35 @@
 import pandas as pd
 import plotly.express as px
 
-file_names = ["./outputs/llama3/has_cause_2018.csv", "./outputs/llama3/has_cause_2019.csv", "./outputs/llama3/has_cause_2020.csv", "./outputs/llama3/has_cause_2021.csv",
-              "./outputs/llama3/has_cause_2022.csv", "./outputs/llama3/has_cause_2023.csv"]
+inflation_has_cause_file_names = ["./outputs/llama3/inflation/1.1.inflation_has_cause_2018.csv",
+                                  "./outputs/llama3/inflation/1.1.inflation_has_cause_2019.csv",
+                                  "./outputs/llama3/inflation/1.1.inflation_has_cause_2020.csv",
+                                  "./outputs/llama3/inflation/1.1.inflation_has_cause_2021.csv",
+                                  "./outputs/llama3/inflation/1.1.inflation_has_cause_2022.csv",
+                                  "./outputs/llama3/inflation/1.1.inflation_has_cause_2023.csv"]
+
+deflation_has_cause_file_names = ["./outputs/llama3/deflation/1.2.deflation_has_cause_2018.csv",
+                                  "./outputs/llama3/deflation/1.2.deflation_has_cause_2019.csv",
+                                  "./outputs/llama3/deflation/1.2.deflation_has_cause_2020.csv",
+                                  "./outputs/llama3/deflation/1.2.deflation_has_cause_2021.csv",
+                                  "./outputs/llama3/deflation/1.2.deflation_has_cause_2022.csv",
+                                  "./outputs/llama3/deflation/1.2.deflation_has_cause_2023.csv"]
+
+change_of_prices_file_names = ["./outputs/llama3/change_in_prices/2.1.change_in_prices_2018.csv",
+                               "./outputs/llama3/change_in_prices/2.1.change_in_prices_2020.csv",
+                               "./outputs/llama3/change_in_prices/2.1.change_in_prices_2022.csv",
+                               "./outputs/llama3/change_in_prices/2.1.change_in_prices_2019.csv",
+                               "./outputs/llama3/change_in_prices/2.1.change_in_prices_2021.csv",
+                               "./outputs/llama3/change_in_prices/2.1.change_in_prices_2023.csv"]
+
+direction_of_change_file_names = ["./outputs/llama3/change_in_prices/2.2.change_direction_2018.csv",
+                                  "./outputs/llama3/change_in_prices/2.2.change_direction_2020.csv",
+                                  "./outputs/llama3/change_in_prices/2.2.change_direction_2022.csv",
+                                  "./outputs/llama3/change_in_prices/2.2.change_direction_2019.csv",
+                                  "./outputs/llama3/change_in_prices/2.2.change_direction_2021.csv",
+                                  "./outputs/llama3/change_in_prices/2.2.change_direction_2023.csv"]
+
+file_names = inflation_has_cause_file_names
 
 stats = []
 df_has_cause = []
@@ -21,16 +48,16 @@ for i, name in enumerate(file_names):
 
 stats_df = pd.DataFrame(stats)
 df_has_cause = pd.concat(df_has_cause)
-df_has_cause.to_csv("./outputs/llama3/has_cause_df.csv")
+df_has_cause.to_csv("./outputs/llama3/inflation/has_cause_df.csv")
 
 fig = px.bar(stats_df, x="Year", y="Count", color="Type",
-             title="Zero-shot classification with Vicuna 1.5 \n (whether or not the cause of inflation is mentioned)")
-fig.write_image("./outputs/llama3/stats_classification_has_cause.png")
+             title="Zero-shot classification with Llama 3 \n (whether or not the cause of inflation is mentioned)")
+fig.write_image("./outputs/llama3/inflation/stats_classification_has_cause.png")
 
 stats_has_cause_df = stats_df.loc[stats_df["Type"] == "#has_cause"]
 fig = px.bar(stats_has_cause_df, x="Year", y="Count",
              title="Distribution of articles mentioning inflation causes")
-fig.write_image("./outputs/llama3/has_cause_distribution.png")
+fig.write_image("./outputs/llama3/inflation/has_cause_distribution.png")
 
 
 
