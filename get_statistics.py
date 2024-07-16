56d159ac8de4665d3ec535da907990c59204535c
Junbo Huang <junbo.huang@uni-hamburg.de>
2024-07-16 23:42:45 +0200

    :sparkles: plot

--- get_statistics.py
+++ get_statistics.py
@@ -29,35 +29,33 @@ direction_of_change_file_names = ["./outputs/llama3/change_in_prices/2.2.change_
                                   "./outputs/llama3/change_in_prices/2.2.change_direction_2021.csv",
                                   "./outputs/llama3/change_in_prices/2.2.change_direction_2023.csv"]
 
-file_names = change_of_prices_file_names
+file_names = inflation_has_cause_file_names
 
 stats = []
 df_has_cause = []
 
 for i, name in enumerate(file_names):
     df = pd.read_csv(name)
-    yes_df = df.loc[df['answer_change_in_prices'].str.startswith("Yes")]
-    no_df = df.loc[df['answer_change_in_prices'].str.startswith("No")]
-    na_df = df.loc[df['answer_change_in_prices'].str.startswith("0")]
+    yes_df = df.loc[df['response'].str.startswith("Yes")]
+    no_df = df.loc[df['response'].str.startswith("No")]
     year = name.split(".csv")[0].split("_")[-1]
     stats.append({"Year": year, "Type": "#has_cause", "Count": len(yes_df)})
     stats.append({"Year": year, "Type": "#no_cause", "Count": len(no_df)})
-    stats.append({"Year": year, "Type": "#na", "Count": len(na_df)})
-    print(f"Year: {year} - #articles: {len(df)} - #has_cause: {len(yes_df)} - #no_cause: {len(no_df)} - #na: {len(na_df)}")
+    print(f"Year: {year} - #articles: {len(df)} - #has_cause: {len(yes_df)} - #no_cause: {len(no_df)}")
     df_has_cause.append(yes_df)
 
 stats_df = pd.DataFrame(stats)
 df_has_cause = pd.concat(df_has_cause)
-df_has_cause.to_csv("./outputs/llama3/change_in_prices/has_cause_df.csv")
+df_has_cause.to_csv("./outputs/llama3/inflation/has_cause_df.csv")
 
 fig = px.bar(stats_df, x="Year", y="Count", color="Type",
-             title="Zero-shot classification with Llama 3 \n (whether or not the cause of change in prices is mentioned)")
+             title="Zero-shot classification with Llama 3 <br>(whether or not the cause of inflation is mentioned)")
 fig.write_image("./outputs/llama3/change_in_prices/stats_classification_has_cause.png")
 
 stats_has_cause_df = stats_df.loc[stats_df["Type"] == "#has_cause"]
 fig = px.bar(stats_has_cause_df, x="Year", y="Count",
-             title="Distribution of articles mentioning causes for change in prices")
-fig.write_image("./outputs/llama3/change_in_prices/has_cause_distribution.png")
+             title="Number of articles mentioning causes for inflation")
+fig.write_image("./outputs/llama3/inflation/has_cause_distribution.png")
 
 
 
