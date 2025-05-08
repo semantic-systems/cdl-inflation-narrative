111c4d4f6ad5a892e174293663a1c86370aef3fc
Max Weinig <max.weinig@uni-hamburg.de>
2025-05-08 11:51:26 +0200

    add econberta

--- annotation/labelstudio/run.sh
+++ annotation/labelstudio/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --gpus-per-node=2 --constraint=48GB
-
+#SBATCH --output=slurm-%j.out  # Speichert die Standardausgabe in einer Datei
+#SBATCH --error=slurm-%j.err   # Speichert die Fehlerausgabe in einer Datei
 
 python ./annotation/labelstudio/agreement.py
