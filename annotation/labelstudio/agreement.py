e7f0a0751ae6740a77b6da11b1e77a2e131dd238
ValweM <max.weinig@uni-hamburg.de>
2025-05-09 11:24:27 +0200

    add econberta

--- annotation/labelstudio/agreement.py
+++ annotation/labelstudio/agreement.py
@@ -17,9 +17,10 @@ from random import randint
 from gliner import GLiNER
 from transformers import EarlyStoppingCallback
 
-os.environ["HF_HOME"] = "./export/huggingface"
+
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 os.environ["PYTORCH_USE_CUDA_DSA"] = "1"
+
 
 
 class InflationNarrative(object):
