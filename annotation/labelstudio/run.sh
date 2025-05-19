44f35dd93caee67e63d5fd70c094947795ccbfa3
Junbo Huang <junbo.huang@uni-hamburg.de>
2025-05-19 23:39:31 +0200

    :sparkles: deep cleaning of repo

--- annotation/labelstudio/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#SBATCH --gpus-per-node=2 --constraint=48GB
-
-python agreement.py
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH --gpus-per-node=2 --constraint=48GB
-
-huggingface-cli login --token $HFTOKENS
-python sequence_classification.py inflation
